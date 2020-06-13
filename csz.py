33a6caf9af528749c7b0c6aaacfb1d180fb79de3
dong67226722 <huafa.dong@groupfu.com>
2020-06-13 23:07:14 +0800

    final ver.

--- csz.py
+++ csz.py
@@ -1,15 +1,21 @@
 import random
 
+start = input("请输入猜数字的开始值:")
+end = input("请输入猜数字的结束值:")
+start = int(start)
+end = int(end)
+
 i = 0
-x = random.randint(1,100)
+x = random.randint(start, end)
 while True:
+	i = i + 1
 	inp = int(input("请猜数字："))
 	if inp == x:
 		print("猜对了！")
+		print("这是第", i,"次猜了！")
 		break
 	elif inp > x:
 		print("比数字大哦，请重猜！")
 	else:
 		print("比数字小，重猜！")
-	i = i + 1
-	print("第", i,"次猜了！")
+	print("这是第", i,"次猜了！")
